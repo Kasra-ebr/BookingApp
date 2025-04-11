8fe15680c412205e456557ada6ba7b7b8f73d8c6
Kasra-ebr <kasraebr1378@gmail.com>
2025-04-10 23:25:00 -0400

    Initial commit

--- src/Component/LocationnList/LocationList.tsx
+++ src/Component/LocationnList/LocationList.tsx
@@ -15,9 +15,9 @@ function LocationList() {
               <img src={loc?.xl_picture_url} alt={loc?.name} />
               <div className="locationItemDesc">
                 <p className="location">{loc.smart_location}</p>
-                <span className="name">{loc.name}</span>
+                <span className="name">{loc?.name}</span>
                 <p className="price">
-                  €{loc.price}
+                  €{loc?.price}
                   <span>/night </span>
                 </p>
               </div>
